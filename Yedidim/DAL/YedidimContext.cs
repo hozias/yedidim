75d4cea7d8f0bf275a8d9476b3b4bc95cce55865
hozias <hozias@outlook.com>
2013-05-31 00:13:20 +0300

    init

---
+++ Yedidim/DAL/YedidimContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Yedidim.Models;
+
+namespace Yedidim.DAL
+{
+    public class YedidimContext : DbContext
+    {
+        public DbSet<Yadid> Yedidim { get; set; }
+        public DbSet<Child> Children { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+    
+}
