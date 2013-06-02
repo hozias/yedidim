5ae06fee684dcbe41647cae0738538455db1ffd8
hozias <hozias@outlook.com>
2013-06-02 23:50:01 +0300

    add-info

---
+++ Yedidim/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YedideyChabad.Authentication.MongoDB;
+using YedideyChabad.Models;
+
+namespace YedideyChabad.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        MongoAuthenticationRepository _a = new MongoAuthenticationRepository();
+        private MongoDatabase _db;
+
+        public AdminController() {
+            _db = _a.GetMongoBatabase();
+        }
+
+        // GET: /Info/
+
+        public ActionResult Info()
+        {
+            int _memCount = (int)_db.GetCollection<Yadid>("Members").Count();
+            int _userCount = (int)_db.GetCollection<Yadid>("Users").Count();
+
+            Info _info = new Info { Members = _memCount, Users = _userCount };
+
+            return View(_info);
+        }
+
+    }
+}
---
+++ Yedidim/Models/Info.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YedideyChabad.Models
+{
+    public class Info
+    {
+        [Display(Name = "ידידם")]
+        public int? Members { get; set; }
+        [Display(Name = "מנהלים")]
+        public int? Users { get; set; }
+    }
+}
